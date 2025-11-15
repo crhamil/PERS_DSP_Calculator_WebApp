cdc59dad7f5504041660efcb5578f8a10bbe4d70
chloe <chloe.hamilcaro@free.fr>
2025-11-15 12:17:05 +0100

    Components details' page
    Implemented components detail's page, based on previous buildings details' page work.
    	- Upgraded the code to make it more lisible
    
    	modified:   src/main/java/com/dsp_calculator/dsp_calculator/components/ComponentController.java
    	modified:   src/main/java/com/dsp_calculator/dsp_calculator/components/ComponentService.java
    	new file:   src/main/resources/templates/component.html
    	modified:   src/main/resources/templates/components.html
    	new file:   src/main/resources/templates/fragments/component-info.html

--- src/main/java/com/dsp_calculator/dsp_calculator/components/ComponentController.java
+++ src/main/java/com/dsp_calculator/dsp_calculator/components/ComponentController.java
@@ -5,11 +5,14 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dsp_calculator.dsp_calculator.models.components.DSPComponent;
 
 @Controller
+@RequestMapping("/components")
 public class ComponentController {
 	@Value("${spring.application.name}")
     String appName;
@@ -17,15 +20,33 @@ public class ComponentController {
 	private final ComponentService service;
 	public ComponentController(ComponentService s) { service = s; }
 	
-	@RequestMapping("/components")
-    public String homePage(Model model) {
+	@RequestMapping({"", "/"})
+    public String componentsListing(Model model) {
 		HashMap<String, DSPComponent> components = service.getAllComponents();
 		int nbComponents = service.getComponentCount();
 		System.out.println("....... " + nbComponents + " composants et ressources dans ComponentController");
 		
-        model.addAttribute("appName", appName);
-        model.addAttribute("components", components);
-        model.addAttribute("nbComponents", nbComponents);
-        return "components";
+		model.addAttribute("appName", appName);
+		model.addAttribute("components", components);
+		model.addAttribute("nbComponents", nbComponents);
+		return "components";
+	}
+	
+	@GetMapping({"/{id}", "/{id}/"})
+    public String componentDetails(Model model, @PathVariable String id) {
+		// TODO: if component not found, return error page
+		DSPComponent component = service.getComponent(id);
+		String componentId = component.getId();
+		String componentName = component.getName();
+		String componentType = component.getClassStringPretty();
+		String componentTypeInfo = service.getComponentClassInfo(component);
+		
+		model.addAttribute("appName", appName);
+		model.addAttribute("c", component);
+		model.addAttribute("cId", componentId);
+		model.addAttribute("cName", componentName);
+		model.addAttribute("cType", componentType);
+		model.addAttribute("infoFragment", componentTypeInfo);
+		return "component";
     }
 }
--- src/main/java/com/dsp_calculator/dsp_calculator/components/ComponentService.java
+++ src/main/java/com/dsp_calculator/dsp_calculator/components/ComponentService.java
@@ -5,10 +5,20 @@ import java.util.HashMap;
 import org.springframework.stereotype.Service;
 
 import com.dsp_calculator.dsp_calculator.models.components.DSPComponent;
+import com.dsp_calculator.dsp_calculator.models.components.FuelComponent;
+import com.dsp_calculator.dsp_calculator.models.components.FuelResource;
+import com.dsp_calculator.dsp_calculator.models.components.Resource;
 import com.dsp_calculator.dsp_calculator.utilities.Game;
 
 @Service
 public class ComponentService {
 	public HashMap<String, DSPComponent> getAllComponents() { return Game.getInstance().getComponents(); }
 	public int getComponentCount() { return getAllComponents().size(); }
+	public DSPComponent getComponent(String id) { return Game.getInstance().getComponent(id); }
+	public String getComponentClassInfo(DSPComponent c) {
+		if (c instanceof FuelResource) return "fuel-resource";
+		else if (c instanceof FuelComponent) return "fuel-component";
+		else if (c instanceof Resource)	return "resource";
+		else return null; /* Nothing for DSPComponent, since there's no additional info to display */
+	}
 }
